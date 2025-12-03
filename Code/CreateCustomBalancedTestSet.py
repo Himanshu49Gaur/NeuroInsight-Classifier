4d5b7ad35f803c3724253827776544e7106c3622
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 00:58:22 +0530

    Update CreateCustomBalancedTestSet.py

--- Code/CreateCustomBalancedTestSet.py
+++ Code/CreateCustomBalancedTestSet.py
@@ -1,1 +1,7 @@
-
+print("Creating custom test set:")
+test_df = pd.concat([X, y], axis=1)
+class_0 = test_df[test_df['SepsisLabel'] == 0].sample(n=5584, random_state=42)
+class_1 = test_df[test_df['SepsisLabel'] == 1].sample(n=5583, random_state=42)
+test_balanced_df = pd.concat([class_0, class_1]).sample(frac=1, random_state=42)
+X_test_balanced = test_balanced_df.drop(columns=['SepsisLabel'])
+y_test_balanced = test_balanced_df['SepsisLabel']
