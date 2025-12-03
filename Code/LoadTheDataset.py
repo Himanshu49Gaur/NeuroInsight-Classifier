a082d5df120bc05a059412926d68dc35806aacba
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 00:37:46 +0530

    Update LoadTheDataset.py

--- Code/LoadTheDataset.py
+++ Code/LoadTheDataset.py
@@ -1,1 +1,8 @@
+import pandas as pd
+
+print("Loading dataset...")
+data_path = "/kaggle/input/prediction-of-sepsis/Dataset.csv"
+df = pd.read_csv(data_path)
+print("First 5 rows of the dataset:")
+print(df.head())
 
