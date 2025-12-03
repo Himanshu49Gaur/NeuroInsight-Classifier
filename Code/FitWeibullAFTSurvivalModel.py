7b703011aaa6a3908e91167951cdd9438aa7aca1
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 01:12:53 +0530

    Update FitWeibullAFTSurvivalModel.py

--- Code/FitWeibullAFTSurvivalModel.py
+++ Code/FitWeibullAFTSurvivalModel.py
@@ -1,1 +1,9 @@
+from lifelines import WeibullAFTFitter
+
+print("Fitting Weibull AFT survival regression model...")
+
+aft = WeibullAFTFitter()
+aft.fit(X_scaled, duration_col='duration', event_col='SepsisLabel')
+
+aft.print_summary()  # Shows coefficients and time ratio effects
 
