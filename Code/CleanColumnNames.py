7cbbdaa556fc9e34b324f7362b54b9c2e2498b0d
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 00:55:43 +0530

    Update CleanColumnNames.py

--- Code/CleanColumnNames.py
+++ Code/CleanColumnNames.py
@@ -1,1 +1,8 @@
+print("Cleaning column names...")
+def clean_column_names(df):
+    return [col.replace('"', '').replace('\\', '').replace(' ', '_')
+                .replace('[', '').replace(']', '')
+                .replace('{', '').replace('}', '')
+                .replace(':', '').replace(',', '') for col in df.columns]
 
+X.columns = clean_column_names(X)
