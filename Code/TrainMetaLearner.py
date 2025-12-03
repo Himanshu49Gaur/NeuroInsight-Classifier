b70313726ad84e8d9f5170bc1e80431998e165ff
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 01:00:40 +0530

    Update TrainMetaLearner.py

--- Code/TrainMetaLearner.py
+++ Code/TrainMetaLearner.py
@@ -1,1 +1,12 @@
-
+print("Training meta-learner...")
+meta_model = XGBClassifier(
+    learning_rate=0.05,
+    n_estimators=1000,
+    max_depth=5,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    eval_metric='auc',
+    scale_pos_weight=1,
+    random_state=42
+)
+meta_model.fit(meta_X, meta_y)
