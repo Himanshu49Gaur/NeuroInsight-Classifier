670297374c0ccfed35421c49786c82699b659100
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 01:20:29 +0530

    Update CompareActualvsPredictedSepsisTimes.py

--- Code/CompareActualvsPredictedSepsisTimes.py
+++ Code/CompareActualvsPredictedSepsisTimes.py
@@ -1,1 +1,9 @@
+print("Displaying sample predictions (actual vs predicted time)...")
+
+comparison_df = df[['SepsisLabel']].copy()
+comparison_df['Predicted_Sepsis_Time'] = predicted_times.round(2)
+comparison_df['Simulated_Observed_Duration'] = df['duration']
+
+# Show top 10 rows
+print(comparison_df.head(10))
 
