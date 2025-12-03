9fca0dfdb6c6871c2c6dbddc5262fa1828bae791
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 01:09:30 +0530

    Update LoadandPreprocessforSurvivalAnalysis.py

--- Code/LoadandPreprocessforSurvivalAnalysis.py
+++ Code/LoadandPreprocessforSurvivalAnalysis.py
@@ -1,1 +1,7 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 
+print("Loading dataset...")
+df = pd.read_csv("/kaggle/input/prediction-of-sepsis/Dataset.csv")
+print(f"Dataset loaded successfully. Shape: {df.shape}")
