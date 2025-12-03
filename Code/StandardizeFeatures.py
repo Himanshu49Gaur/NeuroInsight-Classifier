6bcc85d71b4ae0b42dd1e1c96dec2de8f287a476
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 00:45:19 +0530

    Update StandardizeFeatures.py

--- Code/StandardizeFeatures.py
+++ Code/StandardizeFeatures.py
@@ -1,1 +1,9 @@
+from sklearn.preprocessing import StandardScaler
+
+print("Standardizing features...")
+scaler = StandardScaler()
+X_scaled = pd.DataFrame(scaler.fit_transform(X_imputed), columns=X_imputed.columns)
+
+print("Feature matrix after standardization (first 5 rows):")
+print(X_scaled.head())
 
