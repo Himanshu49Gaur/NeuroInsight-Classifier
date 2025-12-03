bb97e1a2ba8626a739000cd3eceefc2c25ac1201
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-12-04 01:17:11 +0530

    Update VisualizeSurvivalCurvesforaCovariate.py

--- Code/VisualizeSurvivalCurvesforaCovariate.py
+++ Code/VisualizeSurvivalCurvesforaCovariate.py
@@ -1,1 +1,20 @@
+import matplotlib.pyplot as plt
+
+print("Plotting survival function based on a single covariate...")
+
+
+covariate_name = X_predict.columns[0]  
+values_to_test = [-1, 0, 1]  
+aft.plot_partial_effects_on_outcome(
+    covariates=covariate_name,
+    values=values_to_test,
+    cmap='coolwarm'
+)
+
+plt.title(f"Weibull AFT – Survival Curve vs. {covariate_name}")
+plt.xlabel("Time (e.g., hours)")
+plt.ylabel("Survival Probability")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
 
